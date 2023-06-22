1e60007d848b0bd20b484f5f048ae16f28f9a584
Irving Lop <irving.dev.mobi@gmail.com>
2023-06-22 17:18:34 -0600

    adding iOS app functionality

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,14 @@
 import SwiftUI
+import shared
 
 @main
 struct iOSApp: App {
+    
+    let sdk = SpaceXSdk(databaseDriverFactory: DatabaseDriverFactory())
+    
 	var body: some Scene {
 		WindowGroup {
-			ContentView()
+            ContentView(viewModel: .init(sdk: sdk))
 		}
 	}
 }
